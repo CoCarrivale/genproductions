1a5c235470236e09ed71598d2b6d0156fd3fbafe
Roberto Covarelli <roberto.covarelli@cern.ch>
2024-06-17 17:39:22 +0200

    use weights

--- bin/MadGraph5_aMCatNLO/rivet_customize.py
+++ bin/MadGraph5_aMCatNLO/rivet_customize.py
@@ -3,6 +3,8 @@ import FWCore.ParameterSet.Config as cms
 def customise(process):
     process.load('GeneratorInterface.RivetInterface.rivetAnalyzer_cfi')
     process.rivetAnalyzer.AnalysisNames = cms.vstring('COMETA_ZZ_production_analysis_Rivet3')
+    process.rivetAnalyzer.useLHEweights = cms.bool(True)
+    process.rivetAnalyzer.LHECollection = cms.InputTag('externalLHEProducer')
     process.generation_step+=process.rivetAnalyzer
     process.schedule.remove(process.RAWSIMoutput_step)
     return(process)
