5dff10ced6cdea1ef78b944bfb250d4fe57346ea
Roberto Covarelli <roberto.covarelli@cern.ch>
2024-06-18 16:57:43 +0200

    conversion script, not working

---
+++ bin/MadGraph5_aMCatNLO/yodaToPlain.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/perl
+
+# Make fixed format from YODA1 file
+
+$workdir = ".";
+$of = $ARGV[0];
+
+@vars = ("m4l","m3l","mpp","mee","pt4l","ptpp","ptee","ptep","ptmm","yep","ymm","yee","dyee","dypp","dyez","dphiee","dphipp","dree","drpp","cthep","cthmm","cscatt","cosee","cospp");
+
+# Order in yoda file: ("[MUR=2.0_]","[MUR=0.5_]","[MUF=2.0_]","[MUR=2.0_MUF=2.0_]","[MUF=0.5_]","[MUR=0.5_MUF=0.5_]")
+# Final order should be: ("[MUR=0.5_MUF=0.5_]","[MUR=0.5_]","[MUF=0.5_]","[NOMINAL]","[MUF=2.0_]","[MUR=2.0_]","[MUR=2.0_MUF=2.0_]");
+
+open(FH, '>', $of) or die "ciao";;
+    
+foreach $var (@vars) {
+    
+    my @nominal = ();  my $isnominal = 0;
+    my @mur2 = ();  my $ismur2 = 0;
+    my @mur05 = ();  my $ismur05 = 0;
+    my @muf2 = ();  my $ismuf2 = 0;
+    my @murmuf2 = ();  my $ismurmuf2 = 0;
+    my @muf05 = ();  my $ismuf05 = 0;
+    my @murmuf05 = ();  my $ismurmuf05 = 0;
+    
+    print FH "#" . $var . "\n";
+	
+    # retrieve yoda file
+    open(INFILE,"out.yoda") or die "cannot open YODA file";;
+    @log=<INFILE>;
+    close(INFILE);
+
+    # create the seven subfiles with single histograms
+    foreach $line (@log) {
+	if ($line =~ /$(var)/ && $line =~ /BEGIN YODA/ && !($line =~ /RAW/) && !($line =~ /\[/)) {$isnominal = 1;}
+	if ($isnominal == 1 && $line =~ /END YODA/) {$isnominal = 0;}
+	if ($line =~ /$(var)/ && $line =~ /BEGIN YODA/ && !($line =~ /RAW/) && $line =~ /\[MUR=2.0_\]/) {$ismur2 = 1;}
+	if ($ismur2 == 1 && $line =~ /END YODA/) {$ismur2 = 0;}
+	if ($line =~ /$(var)/ && $line =~ /BEGIN YODA/ && !($line =~ /RAW/) && $line =~ /\[MUR=0.5_\]/) {$ismur05 = 1;}
+	if ($ismur05 == 1 && $line =~ /END YODA/) {$ismur05 = 0;}
+	if ($line =~ /$(var)/ && $line =~ /BEGIN YODA/ && !($line =~ /RAW/) && $line =~ /\[MUF=2.0_\]/) {$ismuf2 = 1;}
+	if ($ismuf2 == 1 && $line =~ /END YODA/) {$ismuf2 = 0;}
+	if ($line =~ /$(var)/ && $line =~ /BEGIN YODA/ && !($line =~ /RAW/) && $line =~ /\[MUR=2.0_MUF=2.0_\]/) {$ismurmuf2 = 1;}
+	if ($ismurmuf2 == 1 && $line =~ /END YODA/) {$ismurmuf2 = 0;}
+	if ($line =~ /$(var)/ && $line =~ /BEGIN YODA/ && !($line =~ /RAW/) && $line =~ /\[MUF=0.5_\]/) {$ismuf05 = 1;}
+	if ($ismuf05 == 1 && $line =~ /END YODA/) {$ismuf05 = 0;}
+	if ($line =~ /$(var)/ && $line =~ /BEGIN YODA/ && !($line =~ /RAW/) && $line =~ /\[MUR=0.5_MUF=0.5_\]/) {$ismurmuf05 = 1;}
+	if ($ismurmuf05 == 1 && $line =~ /END YODA/) {$ismurmuf05 = 0;}
+
+	if ($isnominal) {push(@nominal,$line);}
+	if ($ismur2) {push(@mur2,$line);}
+	if ($ismur05) {push(@mur05,$line);}
+	if ($ismuf2) {push(@muf2,$line);}
+	if ($ismurmuf2) {push(@murmuf2,$line);}
+	if ($ismuf05) {push(@muf05,$line);}
+	if ($ismurmuf05) {push(@murmuf05,$line);}
+    }
+
+    my $ientry = 0;
+    foreach $entry (@nominal) {
+	if ($entry =~ /000/) {
+	    @splitentry = split(/ +/, $entry);
+	    print FH $splitentry[0] . "   " . $splitentry[1] . "   " . $splitentry[2] . "   " .  $splitentry[3] . "   ";
+	    @splitmurmuf05 = split(/ +/, $murmuf05[$ientry]);
+	    print FH $splitmurmuf05[2] . "   ";
+	    @splitmur05 = split(/ +/, $mur05[$ientry]);
+	    print FH $splitmur05[2] . "   ";
+	    @splitmuf05 = split(/ +/, $muf05[$ientry]);
+	    print FH $splitmuf05[2] . "   " . $splitentry[2];
+	    @splitmuf2 = split(/ +/, $muf2[$ientry]);
+	    print FH $splitmuf2[2] . "   ";
+	    @splitmur2 = split(/ +/, $mur2[$ientry]);
+	    print FH $splitmur2[2] . "   ";
+	    @splitmurmuf2 = split(/ +/, $murmuf2[$ientry]);
+	    print FH $splitmurmuf2[2] . "\n";
+	$ientry +=1;
+	}
+    }	
+    print FH "\n"
+	
+}
+
+print "Done conversion! Bye bye. \n";
+ 
