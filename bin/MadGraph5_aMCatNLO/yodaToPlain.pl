2efe7adaa25437c4bba62fd5a27bebb9233dff11
Roberto Covarelli <rcovarell@login.uscms.org>
2024-09-11 02:36:22 -0500

    add cross-section

--- bin/MadGraph5_aMCatNLO/yodaToPlain.pl
+++ bin/MadGraph5_aMCatNLO/yodaToPlain.pl
@@ -3,17 +3,22 @@
 # Make fixed format from YODA1 file
 
 $workdir = ".";
-$of = $ARGV[0];
+$inf = $ARGV[0];
+$of  = $ARGV[1];
 
-@vars = ("m4l","m3l","mpp","mee","pt4l","ptpp","ptee","ptep","ptmm","yep","ymm","yee","dyee","dypp","dyez","dphiee","dphipp","dree","drpp","cthep","cthmm","cscatt","cosee","cospp");
+@vars = ("XSEC","m4l","m3l","mpp","mee","pt4l","ptpp","ptee","ptep","ptmm","yep","ymm","yee","dyee","dypp","dyez","dphiee","dphipp","dree","drpp","cthep","cthmm","cscatt","cosee","cospp");
 
 # Order in yoda file: ("[MUR=2.0_]","[MUR=0.5_]","[MUF=2.0_]","[MUR=2.0_MUF=2.0_]","[MUF=0.5_]","[MUR=0.5_MUF=0.5_]")
 # Final order should be: ("[MUR=0.5_MUF=0.5_]","[MUR=0.5_]","[MUF=0.5_]","[NOMINAL]","[MUF=2.0_]","[MUR=2.0_]","[MUR=2.0_MUF=2.0_]");
 
 open(FH, '>', $of) or die "ciao";;
-    
+my $i = 0;
+my $rawentries = 0;    my $israwhisto = 0;
+
 foreach $var (@vars) {
-    
+
+    $i = $i + 1;
+    # Define sub-files for scale variations
     my @nominal = ();  my $isnominal = 0;
     my @mur2 = ();  my $ismur2 = 0;
     my @mur05 = ();  my $ismur05 = 0;
@@ -21,18 +26,21 @@ foreach $var (@vars) {
     my @murmuf2 = ();  my $ismurmuf2 = 0;
     my @muf05 = ();  my $ismuf05 = 0;
     my @murmuf05 = ();  my $ismurmuf05 = 0;
-    
-    print FH "#" . $var . "\n";
+
+    if ($var =~ "XSEC") {print "cross-section: ". $var . "\n" ; print FH "#totxsec\n";}
+    else {print "variable: ". $var . "\n" ; print FH "#" . $var . "\n";}
 	
-    # retrieve yoda file
-    open(INFILE,"out.yoda") or die "cannot open YODA file";;
+    # retrieve complete yoda file
+    open(INFILE,$inf) or die "cannot open YODA file";;
     @log=<INFILE>;
     close(INFILE);
 
-    # create the seven subfiles with single histograms
+    # fill the 7 sub-files with single histograms
     foreach $line (@log) {
 	if ($line =~ m/(${var})/ && $line =~ /BEGIN YODA/ && !($line =~ /RAW/) && !($line =~ /\[/)) {$isnominal = 1;}
 	if ($isnominal == 1 && $line =~ /END YODA/) {$isnominal = 0;}
+	if ($line =~ /m4l/ && $line =~ /BEGIN YODA/ && $line =~ /RAW/ && !($line =~ /\[/)) {$israwhisto = 1;}
+	if ($israwhisto == 1 && $line =~ /END YODA/) {$israwhisto = 0;}
 	if ($line =~ m/(${var})/ && $line =~ /BEGIN YODA/ && !($line =~ /RAW/) && $line =~ /\[MUR=2.0_\]/) {$ismur2 = 1;}
 	if ($ismur2 == 1 && $line =~ /END YODA/) {$ismur2 = 0;}
 	if ($line =~ m/(${var})/ && $line =~ /BEGIN YODA/ && !($line =~ /RAW/) && $line =~ /\[MUR=0.5_\]/) {$ismur05 = 1;}
@@ -53,27 +61,50 @@ foreach $var (@vars) {
 	if ($ismurmuf2) {push(@murmuf2,$line);}
 	if ($ismuf05) {push(@muf05,$line);}
 	if ($ismurmuf05) {push(@murmuf05,$line);}
+	if ($israwhisto && $line =~ /Total/) {@splitline = split('\t', $line); $rawentries = $splitline[2];} 
     }
 
     my $ientry = 0;
-    foreach $entry (@nominal) {
-	if ($entry =~ /000/ && !($entry =~ /Total/) && !($entry =~ /flow/) && !($entry =~ /ScaledBy/)) {
-	    @splitentry = split('\t', $entry);
-	    print FH $splitentry[0] . "\t" . $splitentry[1] . "\t" . $splitentry[2] . "\t" .  $splitentry[3] . "\t";
-	    @splitmurmuf05 = split('\t', $murmuf05[$ientry]);
-	    print FH $splitmurmuf05[2] . "\t";
-	    @splitmur05 = split('\t', $mur05[$ientry]);
-	    print FH $splitmur05[2] . "\t";
-	    @splitmuf05 = split('\t', $muf05[$ientry]);
-	    print FH $splitmuf05[2] . "\t" . $splitentry[2] . "\t";
-	    @splitmuf2 = split('\t', $muf2[$ientry]);
-	    print FH $splitmuf2[2] . "\t";
-	    @splitmur2 = split('\t', $mur2[$ientry]);
-	    print FH $splitmur2[2] . "\t";
-	    @splitmurmuf2 = split('\t', $murmuf2[$ientry]);
-	    print FH $splitmurmuf2[2] . "\n";
+    if ($var =~ "XSEC") {
+	foreach $entry (@nominal) {
+	    if ($entry =~ /000/ && !($entry =~ /ScaledBy/)) {   ## only actual cross-section
+		@splitentry = split('\t', $entry);
+		print FH "0.00000 \t 1.00000 \t" . $splitentry[0]*$rawentries/250. . "\t" . $splitentry[1]*$rawentries/250. . "\t";
+		@splitmurmuf05 = split('\t', $murmuf05[$ientry]);
+		print FH $splitmurmuf05[0]*$rawentries/250. . "\t";
+		@splitmur05 = split('\t', $mur05[$ientry]);
+		print FH $splitmur05[0]*$rawentries/250. . "\t";
+		@splitmuf05 = split('\t', $muf05[$ientry]);
+		print FH $splitmuf05[0]*$rawentries/250. . "\t" . $splitentry[0]*$rawentries/250. . "\t";
+		@splitmuf2 = split('\t', $muf2[$ientry]);
+		print FH $splitmuf2[0]*$rawentries/250. . "\t";
+		@splitmur2 = split('\t', $mur2[$ientry]);
+		print FH $splitmur2[0]*$rawentries/250. . "\t";
+		@splitmurmuf2 = split('\t', $murmuf2[$ientry]);
+		print FH $splitmurmuf2[0]*$rawentries/250. . "\n";
+	    }
+	    $ientry +=1;
 	}
-	$ientry +=1;
+    } else {
+	foreach $entry (@nominal) {
+	    if ($entry =~ /000/ && !($entry =~ /Total/) && !($entry =~ /flow/) && !($entry =~ /ScaledBy/)) {   ## only actual histo entries
+		@splitentry = split('\t', $entry);
+		print FH $splitentry[0] . "\t" . $splitentry[1] . "\t" . $splitentry[2] . "\t" .  $splitentry[3] . "\t";
+		@splitmurmuf05 = split('\t', $murmuf05[$ientry]);
+		print FH $splitmurmuf05[2] . "\t";
+		@splitmur05 = split('\t', $mur05[$ientry]);
+		print FH $splitmur05[2] . "\t";
+		@splitmuf05 = split('\t', $muf05[$ientry]);
+		print FH $splitmuf05[2] . "\t" . $splitentry[2] . "\t";
+		@splitmuf2 = split('\t', $muf2[$ientry]);
+		print FH $splitmuf2[2] . "\t";
+		@splitmur2 = split('\t', $mur2[$ientry]);
+		print FH $splitmur2[2] . "\t";
+		@splitmurmuf2 = split('\t', $murmuf2[$ientry]);
+		print FH $splitmurmuf2[2] . "\n";
+	    }
+	    $ientry +=1;
+	}
     }	
     print FH "\n"
 	
